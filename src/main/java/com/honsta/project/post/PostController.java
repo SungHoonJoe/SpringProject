4bce6a389ef41e63a06dbd7f3ce3fe27d0a70c47
DESKTOP-NJKHAHU\user <mile@naver.com>
2022-01-26 21:46:59 +0900

    [조성훈] 1/26 두번째 수정

--- src/main/java/com/honsta/project/post/PostController.java
+++ src/main/java/com/honsta/project/post/PostController.java
@@ -1,5 +1,16 @@
 package com.honsta.project.post;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/post")
 public class PostController {
+	
+	@GetMapping("/list_view")
+	public String listView() {
+		return "post/listView";
+	}
 
 }
